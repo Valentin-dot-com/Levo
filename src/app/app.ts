936f508ec29f4e15d31dc1af0edf3094766f093d
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2026-01-15 21:12:14 +0100

    fix: iOS resizing issues when opening keyboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/authenticate';
 import { AsyncPipe } from '@angular/common';
@@ -8,11 +8,24 @@ import { LoaderComponent } from './components/loader/loader';
   selector: 'app-root',
   imports: [RouterOutlet, AsyncPipe, LoaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('Levo');
   private auth = inject(AuthService);
 
   isLoading$ = this.auth.isLoading$;
+
+  ngOnInit() {
+    if (this.isiOS() && window.visualViewport) {
+      console.log('Ios resize')
+      window.visualViewport.addEventListener('resize', () => {
+        document.body.style.height = window.visualViewport!.height + 'px';
+      });
+    }
+  }
+
+  private isiOS(): boolean {
+    return /iPhone|iPad|iPod/.test(navigator.userAgent);
+  }
 }
