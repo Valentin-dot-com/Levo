03ebbedcc04152297f8800c65dd9d766e00704b6
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2026-01-19 18:03:56 +0100

    style: desktop layout for creating new boards

--- src/app/pages/boards/boards.ts
+++ src/app/pages/boards/boards.ts
@@ -4,10 +4,11 @@ import { RouterLink } from '@angular/router';
 import { BoardService } from '../../services/boards';
 import { CalendarService } from '../../services/calendars';
 import { SharedIconComponent } from '../../icons/sharedIcon';
+import { NewBoardComponent } from '../../components/forms/new-board/new-board';
 
 @Component({
   selector: 'app-boards',
-  imports: [CommonModule, RouterLink, SharedIconComponent],
+  imports: [CommonModule, RouterLink, SharedIconComponent, NewBoardComponent],
   templateUrl: './boards.html',
   styleUrl: './boards.scss',
 })
@@ -17,9 +18,19 @@ export class BoardsComponent {
 
   boards = this.boardsService.boards;
   loading = signal(false);
+  openCreate = signal(false);
 
   getCalendar(calId: string) {
     const calendar = this.calendarService.calendars().find((cal) => cal.id === calId);
     return calendar || null;
+  }
+
+  openCreateNew() {
+    this.openCreate.set(true);
+  }
+
+  close() {
+    this.openCreate.set(false);
+    this.openCreate.set(false);
   }
 }
